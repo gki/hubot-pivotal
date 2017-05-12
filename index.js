e565c34f12749db3e210d076e6f0fc4beadd4008
Gen Takeda <gen@lemona.de>
2017-05-12 21:42:51 +0900

    use defalt src path

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
-var fs = require('fs');
 var path = require('path');
 
-module.exports = function(robot, scripts) {
-  var scriptsPath;
-  scriptsPath = path.resolve(__dirname, 'src');
-  return fs.exists(scriptsPath, function(exists) {
-    var i, len, ref, results, script;
-    if (exists) {
-      ref = fs.readdirSync(scriptsPath);
-      results = [];
-      for (i = 0, len = ref.length; i < len; i++) {
-        script = ref[i];
-        if ((scripts != null) && indexOf.call(scripts, '*') < 0) {
-          if (indexOf.call(scripts, script) >= 0) {
-            results.push(robot.loadFile(scriptsPath, script));
-          } else {
-            results.push(void 0);
-          }
-        } else {
-          results.push(robot.loadFile(scriptsPath, script));
-        }
-      }
-      return results;
-    }
-  });
+module.exports = function(robot) {
+  robot.load(path.join(__dirname, '/src/scripts/'));
 };
