d59525be08483940fa6a8045120c7783aa437799
Gen Takeda <gen@lemona.de>
2017-06-27 00:08:43 +0900

    add error test cases

--- test/user-tickets-test.js
+++ test/user-tickets-test.js
@@ -26,7 +26,7 @@ describe("Test for user tickets feature", function() {
 
     // test 
     it ("show user's all tickets.", function(done) {
-
+        fail();
     });
 
     it ("show user's all tickets (there is no user's ticket)", function(done) {
@@ -34,11 +34,58 @@ describe("Test for user tickets feature", function() {
     });
     
     it ("show user's all tickets w/o user info.", function(done) {
-        fail();
+        let dummyRobot = new DummyRobot();
+        let spyRespond = sinon.spy(dummyRobot, "captureSend");
+
+        // test
+        dummyRobot.testRun(targetScript,
+            "show my pivotal tickets",
+            function (reply) {
+                // check
+                try {
+                    // response
+                    chai.expect(reply).to.have.string("no liked pivotal user");
+                    chai.expect(reply).to.not.have.string("Done");
+                    chai.expect(reply).to.not.have.string("http");
+                    chai.expect(reply).to.not.have.string("point");
+
+                } catch (err) {
+                    done(err);
+                    return;
+                }
+                done();
+            });
     });
 
     it ("show user's all tickets w/o project info.", function(done) {
-        fail();
+        let dummyRobot = new DummyRobot();
+        let spyRespond = sinon.spy(dummyRobot, "captureSend");
+
+        let registeredData = {};
+        registeredData[dummyRobot.userName] = {
+            pv_id   : "111",
+            pv_name : "Ieyasu Tokugawa"
+        };
+        dummyRobot.brain.set(TestConst.BRAIN_KEY_USERS, registeredData);
+
+        // test
+        dummyRobot.testRun(targetScript,
+            "show my pivotal tickets",
+            function (reply) {
+                // check
+                try {
+                    // response
+                    chai.expect(reply).to.have.string("no project info");
+                    chai.expect(reply).to.not.have.string("Done");
+                    chai.expect(reply).to.not.have.string("http");
+                    chai.expect(reply).to.not.have.string("point");
+
+                } catch (err) {
+                    done(err);
+                    return;
+                }
+                done();
+            });
     });
 
 })
