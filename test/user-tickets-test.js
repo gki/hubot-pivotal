8eba82aaaad4cb28046643edb47757fe01450eae
Gen Takeda <gen@lemona.de>
2017-07-01 22:13:27 +0900

    implement test cade

--- test/user-tickets-test.js
+++ test/user-tickets-test.js
@@ -26,11 +26,83 @@ describe("Test for user tickets feature", function() {
 
     // test 
     it ("show user's all tickets.", function(done) {
-        fail();
+        let dummyRobot = new DummyRobot();
+        let spyRespond = sinon.spy(dummyRobot, "captureSend");
+        let testResponse = dummyUserTicketsJson;
+        dummyRobot.addHttpMockResponse(() => {
+            return JSON.stringify(testResponse);
+        });
+
+        _setupDummyUserToBbrain(dummyRobot);
+        _setupDummyProjectToBbrain(dummyRobot);
+
+        // test
+        var actualCheckCount = 0;
+        dummyRobot.testRun(targetScript,
+            "show my pivotal tickets",
+            function (reply) {
+                // check
+                actualCheckCount++;
+                try {
+                    switch (actualCheckCount) {
+                        case 1:
+                            chai.expect(reply).to.have.string(`${testResponse.stories.total_hits} tickets`);
+                            break;
+                        case 2: {
+                            let story = testResponse.stories.stories[0];
+                            chai.expect(reply).to.have.string(story.name);
+                            chai.expect(reply).to.have.string(story.id);
+                            chai.expect(reply).to.have.string(story.url);
+                            chai.expect(reply).to.have.string(story.story_type);
+                            chai.expect(reply).to.have.string(story.current_state);
+                            chai.expect(reply).to.have.string(story.estimate);
+                            break;
+                        }
+                        case 3: {
+                            let story = testResponse.stories.stories[1];
+                            chai.expect(reply).to.have.string(story.name);
+                            chai.expect(reply).to.have.string(story.id);
+                            chai.expect(reply).to.have.string(story.url);
+                            chai.expect(reply).to.have.string(story.story_type);
+                            chai.expect(reply).to.have.string(story.current_state);
+                            chai.expect(reply).to.have.string(story.estimate);
+                            done()
+                            break;
+                        }
+                        default:
+                            done(new Error("too much check than expected."))    
+                    }
+                } catch (err) {
+                    done(err);
+                    return;
+                }
+            });
     });
 
-    it ("show user's all tickets (there is no user's ticket)", function(done) {
-        fail();
+    it ("show user's all tickets (there is no user's ticket)", function() {
+        let dummyRobot = new DummyRobot();
+        let spyRespond = sinon.spy(dummyRobot, "captureSend");
+
+        dummyRobot.addHttpMockResponse(() => {
+            return JSON.stringify({
+                "stories":{
+                    "stories":[],
+                    "total_hits":0,
+                    "total_hits_with_done":2,
+                    "total_points":0,
+                    "total_points_completed":0
+                },
+                "query":"owner:111 "});
+        });
+
+        _setupDummyUserToBbrain(dummyRobot);
+        _setupDummyProjectToBbrain(dummyRobot);
+
+        // test
+        dummyRobot.testRun(targetScript,
+            "show my pivotal tickets");
+        chai.expect(spyRespond).to.not.be.called;
+
     });
     
     it ("show user's all tickets w/ http error response.", function() {
@@ -41,21 +113,8 @@ describe("Test for user tickets feature", function() {
             return new Error('dummy errro.');
         });
 
-        let registeredData = {};
-        registeredData[dummyRobot.userName] = {
-            pv_id   : "111",
-            pv_name : "Ieyasu Tokugawa"
-        };
-        dummyRobot.brain.set(TestConst.BRAIN_KEY_USERS, registeredData);
-        let testData = {
-            1111 : {
-                id  : 1111,
-                name: 'project A',
-                url: 'http//test/a',
-                description: 'description for A'
-            }
-        };
-        dummyRobot.brain.set(TestConst.BRAIN_KEY_PROJECTS, testData);
+        _setupDummyUserToBbrain(dummyRobot);
+        _setupDummyProjectToBbrain(dummyRobot);
 
         // test
         dummyRobot.testRun(targetScript,
@@ -65,28 +124,15 @@ describe("Test for user tickets feature", function() {
     });
 
     it ("show user's all tickets w/ pivotal api error.", function() {
-         let dummyRobot = new DummyRobot();
+        let dummyRobot = new DummyRobot();
         let spyRespond = sinon.spy(dummyRobot, "captureSend");
 
         dummyRobot.addHttpMockResponse(() => {
             return JSON.stringify({code :'unfound_resource'});
         });
 
-        let registeredData = {};
-        registeredData[dummyRobot.userName] = {
-            pv_id   : "111",
-            pv_name : "Ieyasu Tokugawa"
-        };
-        dummyRobot.brain.set(TestConst.BRAIN_KEY_USERS, registeredData);
-        let testData = {
-            1111 : {
-                id  : 1111,
-                name: 'project A',
-                url: 'http//test/a',
-                description: 'description for A'
-            }
-        };
-        dummyRobot.brain.set(TestConst.BRAIN_KEY_PROJECTS, testData);
+        _setupDummyUserToBbrain(dummyRobot);
+        _setupDummyProjectToBbrain(dummyRobot);
 
         // test
         dummyRobot.testRun(targetScript,
@@ -123,13 +169,7 @@ describe("Test for user tickets feature", function() {
         let dummyRobot = new DummyRobot();
         let spyRespond = sinon.spy(dummyRobot, "captureSend");
 
-        let registeredData = {};
-        registeredData[dummyRobot.userName] = {
-            pv_id   : "111",
-            pv_name : "Ieyasu Tokugawa"
-        };
-        dummyRobot.brain.set(TestConst.BRAIN_KEY_USERS, registeredData);
-
+        _setupDummyUserToBbrain(dummyRobot);
         // test
         dummyRobot.testRun(targetScript,
             "show my pivotal tickets",
@@ -149,5 +189,79 @@ describe("Test for user tickets feature", function() {
                 done();
             });
     });
+});
 
-})
+function _setupDummyUserToBbrain(dummyRobot) {
+    let registeredData = {};
+    registeredData[dummyRobot.userName] = {
+        pv_id   : "111",
+        pv_name : "Ieyasu Tokugawa"
+    };
+    dummyRobot.brain.set(TestConst.BRAIN_KEY_USERS, registeredData);
+    return registeredData;
+}
+
+function _setupDummyProjectToBbrain(dummyRobot) {
+    let testData = {
+        1111 : {
+            id  : 1111,
+            name: 'project A',
+            url: 'http//test/a',
+            description: 'description for A'
+        }
+    };
+    dummyRobot.brain.set(TestConst.BRAIN_KEY_PROJECTS, testData);
+    return testData;
+}
+
+const dummyUserTicketsJson = {
+  "stories":{
+    "stories":[
+      {
+        "kind":"story",
+        "id":2222,
+        "created_at":"2017-06-23T09:58:12Z",
+        "updated_at":"2017-06-28T05:55:07Z",
+        "accepted_at":"2017-06-28T05:55:06Z",
+        "story_type":"chore",
+        "name":"update test environment",
+        "current_state":"accepted",
+        "requested_by_id":111,
+        "url":"https://www.pivotaltracker.com/story/show/2222",
+        "project_id":1111,
+        "owner_ids":[
+          111
+        ],
+        "labels":[
+
+        ],
+        "owned_by_id":111
+      },
+      {
+        "kind":"story",
+        "id":3333,
+        "created_at":"2017-02-24T03:34:39Z",
+        "updated_at":"2017-06-29T03:26:54Z",
+        "estimate":0,
+        "story_type":"bug",
+        "name":"App crashed when user saved Foo.",
+        "current_state":"unscheduled",
+        "requested_by_id":111,
+        "url":"https://www.pivotaltracker.com/story/show/333",
+        "project_id":1111,
+        "owner_ids":[
+          111
+        ],
+        "labels":[
+
+        ],
+        "owned_by_id":111
+      }
+    ],
+    "total_points":0,
+    "total_points_completed":0,
+    "total_hits":2,
+    "total_hits_with_done":10
+  },
+  "query":"owner:111 "
+};
